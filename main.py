bd039c110a093487d8f82236535b6d7dbca78f9e
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-05-15 12:26:48 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import pandas as ps
+from fpdf import FPDF
+
+
+class Invoice:
+    def make_pdf(self):
+        pass
+
+
+def main():
+    get_dataframe()
+    find_item()
+    add_to_reciepe()
+    make_pdf()
+
+
+if __name__ == "__main__":
+    main()
